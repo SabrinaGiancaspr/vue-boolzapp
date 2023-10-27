32ce4cd4036533e864c6d4d8e0f76e50d12b89cd
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-27 13:54:01 +0200

    add v-bind img avatar

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ app({
         },
         {
           name: 'Fabio',
-          avatar: './img/avatar_2.png',
+          avatar: './img/avatar_2.jpg',
           visible: true,
           messages: [
           {
@@ -49,7 +49,7 @@ app({
           },
           {
           name: 'Samuele',
-          avatar: './img/avatar_3.png',
+          avatar: './img/avatar_3.jpg',
           visible: true,
           messages: [
           {
@@ -71,7 +71,7 @@ app({
           },
           {
           name: 'Alessandro B.',
-          avatar: './img/avatar_4.png',
+          avatar: './img/avatar_4.jpg',
           visible: true,
           messages: [
           {
@@ -88,7 +88,7 @@ app({
           },
           {
           name: 'Alessandro L.',
-          avatar: './img/avatar_5.png',
+          avatar: './img/avatar_5.jpg',
           visible: true,
           messages: [
           {
@@ -105,7 +105,7 @@ app({
           },
           {
           name: 'Claudia',
-          avatar: './img/avatar_6.png',
+          avatar: './img/avatar_6.jpg',
           visible: true,
           messages: [
           {
@@ -127,7 +127,7 @@ app({
           },
           {
           name: 'Federico',
-          avatar: './img/avatar_7.png',
+          avatar: './img/avatar_7.jpg',
           visible: true,
           messages: [
           {
@@ -144,7 +144,7 @@ app({
           },
           {
           name: 'Davide',
-          avatar: './img/avatar_8.png',
+          avatar: './img/avatar_8.jpg',
           visible: true,
           messages: [
           {
